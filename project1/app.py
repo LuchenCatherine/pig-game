bcc5d852f60c2886b688488c8c9b127497457529
catherine <luchen_catherine@outlook.com>
2022-12-15 19:32:40 -0500

    update

--- project1/app.py
+++ project1/app.py
@@ -9,8 +9,12 @@ app = Flask(__name__)
 def index():  # put application's code here
 
     action = np.load('one_die_action.npy')
+    action_simul_normal = np.load('one_dice_simul_vs25_action.npy')
+    action_simul_hard = np.load('optimal_action.npy')
     json_array = json.dumps(action.tolist())
-    return render_template('index.html', data=json_array)
+    json_array_simul_normal = json.dumps(action_simul_normal.tolist())
+    json_array_simul_hard = json.dumps(action_simul_hard.tolist())
+    return render_template('index2.html', data=json_array, normal=json_array_simul_normal, hard=json_array_simul_hard)
 
 
 if __name__ == '__main__':
